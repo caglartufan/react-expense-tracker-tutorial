0500f2784efc9856354cba51b173a8ba7eea0839
Muhammed Çağlar TUFAN <caglartufan09@gmail.com>
2023-02-24 00:36:46 +0300

    Completed section and homework about filtering expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,28 @@
+import { useState } from 'react';
+
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 const Expenses = ({ expenses }) => {
-    console.log(expenses);
+    const [filter, setFilter] = useState(2021);
+
+    const filterChangeHandler = filterValue => {
+        setFilter(filterValue);
+    };
+
     return (
         <Card className="expenses">
+            <ExpensesFilter filterValue={filter} onFilterChange={filterChangeHandler} />
             {
-                expenses.map(
-                    expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-                )
+                expenses
+                    .filter(
+                        expense => expense.date.getFullYear() === filter
+                    )
+                    .map(
+                        expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+                    )
             }
         </Card>
     );
